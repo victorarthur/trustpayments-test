11d6c3f55d9c06398fe0bbd40f23333ae2b06026
Victor Arthur <hello@victorarthur.com>
2024-06-26 14:21:22 +0200

    WIP

--- app/Http/Controllers/TrustPaymentsController.php
+++ app/Http/Controllers/TrustPaymentsController.php
@@ -6,42 +6,42 @@ use Illuminate\Http\Request;
 
 class TrustPaymentsController extends Controller
 {
-    /*
-    Hi Victor
-
-I've gone to your url and can see this is your jwt value;
-
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImFjY291bnR0eXBlZGVzY3JpcHRpb24iOiJFQ09NIiwiYmFzZWFtb3VudCI6IjEwNTAiLCJjdXJyZW5jeWlzbzNhIjoiR0JQIiwic2l0ZXJlZmVyZW5jZSI6InRlc3Rfbm9vbGFiczEyNzIxNiIsInJlcXVlc3R0eXBlZGVzY3JpcHRpb25zIjpbIlRIUkVFRFFVRVJZIiwiQVVUSCJdLCJiaWxsaW5nZmlyc3RuYW1lIjoiQmVydGFsYW4iLCJiaWxsaW5nbGFzdG5hbWUiOiJTXHUwMGUxbmRvciIsImxvY2FsZSI6ImVuX0dCIn0sImlhdCI6MTcxOTQwMjIwNCwiaXNzIjoiand0QG5vb2xhYnN0cmFkaW5nLmNvbSJ9.xkzz8d043Uhh36eZ5p71RBzd6j90e7Z+Hr4AgSFRxVQ=
-
-upon checking it, it has not been created using base64url encoding so it is failing.
-
-Please can you ensure you are creating the JWT in Base64Url encoding as per our guide: JSON Web Token
-
-HMACSHA256(base64UrlEncode(header) + "." + base64UrlEncode(payload), secret)
-
-The final step is to ensure the signature is Base64URL encoded.
-
-Please can you check your encoding so we can test it again
-
-Kind Regards
-Mark Gennoe
-Senior Technical Support Officer
-*/
-    public function generateJWT()
+    private function base64UrlEncode($data)
     {
-        $jwtFieldsData = json_decode(request()->getContent(), true);
-        $header = json_encode(['alg' => 'HS256', 'typ' => 'JWT']);
-        $header = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
-
-        $payload = json_encode($jwtFieldsData);
-        $payload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
-
-        $secret = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode('60-2771952b06f6403e7fc854ccff7a778317f79626212e659ac1b45e7e8822a78c'));
-
-        return hash_hmac('sha256',$header . '.' . $payload, $secret, true);
+        // Base64 encode the data
+        $base64 = base64_encode($data);
+        // Replace '+' with '-', '/' with '_', and remove '='
+        return str_replace(['+', '/', '='], ['-', '_', ''], $base64);
     }
 
-    public function processAuth() {
+    private function createJwtSignature($header, $payload, $secret)
+    {
+        // Encode the header and payload
+        $encodedHeader = $this->base64UrlEncode($header);
+        $encodedPayload = $this->base64UrlEncode($payload);
+
+        // Concatenate the encoded header and payload with a dot
+        $data = $encodedHeader . "." . $encodedPayload;
+
+        // Create the HMACSHA256 signature
+        $signature = hash_hmac('sha256', $data, $secret, true);
+
+        // Encode the signature using base64UrlEncode
+        return $this->base64UrlEncode($signature);
+    }
+
+    public function generateJWT()
+    {
+        $header = json_encode(['alg' => 'HS256', 'typ' => 'JWT']);
+        $payload = json_encode(json_decode(request()->getContent(), true));
+        $secret = '60-2771952b06f6403e7fc854ccff7a778317f79626212e659ac1b45e7e8822a78c';
+        $signature = $this->createJwtSignature($header, $payload, $secret);
+
+        return $this->base64UrlEncode($header) . "." . $this->base64UrlEncode($payload) . "." . $signature;
+    }
+
+    public function processAuth()
+    {
         dd(request()->all());
         // it never gets to this point
     }
