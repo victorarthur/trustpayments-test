5555a981cc98def180e01b76156ddc95b89f6763
Berci <ilyen85@gmail.com>
2024-06-28 17:44:02 +0200

    Add the most common error messages

--- app/Http/Controllers/TrustPaymentsController.php
+++ app/Http/Controllers/TrustPaymentsController.php
@@ -66,6 +66,40 @@ class TrustPaymentsController extends Controller
         return ['valid' => false];
     }
 
+    private function retrieveFailiure(String $error_code, String $error_data = '',) {
+        if($auth_result['errorcode'] == '30000') {
+            // Implementation error: Indicates invalid data has been submitted within the payload of the JWT
+            $failiure_reason = "The transaction failed due to invalid transaction information.";
+            if($error_data) {
+                $failiure_reason = $failiure_reason . ' Possible error in the ' . $error_data . ' field.';
+            }
+            return $failiure_reason;
+        }
+
+        if(in_array($auth_result['errorcode'], ['60010', '60034', '99999'])) {
+            //This can be due to a communication problem with a bank or third party.
+            // We recommend informing the customer of the problem and to contact you to query the issue. You will need to contact our Support Team, providing a copy of the entire request submitted and response returned, and we will contact the relevant parties to establish the status of the request. In the interest of security, ensure you omit or mask sensitive field values, such as card details.
+            return "The transaction failed due to a communication error. Please verify whether the amount has been charged. If necessary, contact our customer service for assistance with a refund.";
+        }
+        if($auth_result['errorcode'] == '60022') {
+            //The customer was prompted for authentication, but failed this part of the process, meaning the transaction was not authorised.
+            return "The transaction failed due autohorization code is not provided. Please attempt the transaction again, ensuring the autohorization code is entered correctly, or use an alternative payment method.";
+        }
+        if($auth_result['errorcode'] == '70000') {
+            //Authorisation for the payment was attempted but was declined by the issuing bank.
+            return "The transaction failed due autohorization is attempted but the autohorization code is invalid. Please attempt the transaction again, ensuring the autohorization code is entered correctly, or use an alternative payment method.";
+        }
+        if($auth_result['errorcode'] == '71000') {
+            //https://help.trustpayments.com/hc/en-us/articles/4403193781265-Why-has-my-payment-been-returned-a-71000-Soft-decline-response
+            //https://help.trustpayments.com/hc/en-us/articles/6622494125585-Testing-Soft-declines
+            // Include parenttransactionreference to the original authorisation that was soft declined (reference the AUTH, not the THREEDQUERY).
+            // @TODO: @victorarthur Re-send the transaction with parenttransactionreference included
+            return "The card issuer declined the request due to absence of Strong Customer Authentication (SCA). You will need to resubmit the transaction with the necessary EMV 3DS authentication.";
+        }
+
+        // if error message is still not returned, look it up in every error messages:
+        // https://webapp.securetrading.net/errorcodes.html
+    }
 
     public function processAuth()
     {
@@ -82,14 +116,38 @@ class TrustPaymentsController extends Controller
             ]);
         }
 
-        return ($response['errorcode'] == 0) ? response()->json([
-            'success' => true,
-            'transactionId' => $response['transactionreference'],
-            'payload' => $is_jwt_valid['payload'],
-
-        ]) : response()->json([
+        //Request type: You will need to check the requesttypedescription returned in the response. Only values of “AUTH” indicate the authorisation of a payment.
+        $auth_result = $is_jwt_valid['payload']['response']->first(function ($item) {
+            return $item['requesttypedescription'] === 'AUTH';
+        });
+        
+        $failiure_reason = '';
+        if(isset($auth_result)) {
+            if($auth_result['errorcode'] == '0') {
+                // Paid for the product
+                return response()->json([
+                    'success' => true,
+                    'transactionId' => $response['transactionreference'],
+                    'payload' => $is_jwt_valid['payload'],
+                ]);
+            }
+            else {
+                $failiure_reason = $this->retrieveFailiure(
+                    $auth_result['errorcode'], 
+                    isset($auth_result['errordata']) ? $auth_result['errordata'] : ''
+                );
+            }
+            
+        }
+        
+        // response has no authentication, although the transaction might be a success, the customer not paid for the product(s)
+        $sum_message = $response['errormessage'];
+        if($failiure_reason) {
+            $sum_message = $sum_message . '. Failiure response: ' . $failiure_reason;
+        }
+        return response()->json([
             'success' => false,
-            'message' => $response['errormessage'],
+            'message' => $sum_message,
         ]);
 
     }
