6d4bb4a5b2b7c96cb4b072dc30cd03eb47a4b93f
Berci <ilyen85@gmail.com>
2024-06-28 14:56:10 +0200

    Add event handlers for testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,32 +44,64 @@
 			'locale': 'en_GB'
 		},
 		'iat': parseInt(Date.now() / 1000),
-        'iss': 'jwt@noolabstrading.com',
-    }
+		'iss': 'jwt@noolabstrading.com',
+	}
 
-    let encoded_data = JSON.stringify(jwt_data);
+	let encoded_data = JSON.stringify(jwt_data);
 
-    let xcsrf = document.querySelector('meta[name="csrf-token"]')?.content || ''
-    fetch('jwt-generate', {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'X-CSRF-TOKEN': xcsrf,
-        },
-        body: encoded_data,
-    })
-        .then(function (res) {
-            return res.text();
-        })
-        .then(generated_jwt => {
-            console.log(`TrustPayments - generated_jwt: ${JSON.stringify(generated_jwt)}`)
+	let xcsrf = document.querySelector('meta[name="csrf-token"]')?.content || ''
+	fetch('jwt-generate', {
+		method: "POST",
+		mode: "cors",
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'X-CSRF-TOKEN': xcsrf,
+		},
+		body: encoded_data,
+	})
+	.then(function (res) {
+		return res.text();
+	})
+	.then(generated_jwt => {
+		console.log(`TrustPayments - generated_jwt: ${JSON.stringify(generated_jwt)}`)
 
-            var st = SecureTrading({
-                jwt: generated_jwt
-            });
-            st.Components();
-        })
+		var st = SecureTrading({
+			jwt: generated_jwt
+		});
+		st.Components();
+
+		st.on('paymentPreCheck', (data) => {
+			console.log('TrustPayments - st paymentPreCheck event - data: ', JSON.stringify(data))
+		})
+		//Called when the payment initialisation has started.
+		st.on('paymentInitStarted', (data) => {
+			console.log('TrustPayments - st paymentInitStarted event - data: ', JSON.stringify(data))
+		})
+		//Called when the payment initialisation has successfully completed.
+		st.on('paymentInitCompleted', (data) => {
+			console.log('TrustPayments - st paymentInitCompleted event - data: ', JSON.stringify(data))
+		})
+		//Called when the payment initialisation has encountered an error.
+		st.on('paymentInitFailed', (data) => {
+			console.log('TrustPayments - st paymentInitFailed event - data: ', JSON.stringify(data))
+		})
+		//Called when the payment process has started.
+		st.on('paymentStarted', (data) => {
+			console.log('TrustPayments - st paymentStarted event - data: ', JSON.stringify(data))
+		})
+		//Called when the payment has reached the gateway.
+		st.on('paymentCompleted', (data) => {
+			console.log('TrustPayments - st paymentCompleted event - data: ', JSON.stringify(data))
+		})
+		//Called when the payment has failed.
+		st.on('paymentFailed', (data) => {
+			console.log('TrustPayments - st paymentFailed event - data: ', JSON.stringify(data))
+		})
+		//Called when the user has cancelled the payment before it has reached the gateway.
+		st.on('paymentCanceled', (data) => {
+			console.log('TrustPayments - st paymentCanceled event - data: ', JSON.stringify(data))
+		})
+	})
 </script>
 </body>
 </html>
